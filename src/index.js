bb5bbe9532fcf005ac988e75173658ec9870f436
Nathan Gentry <gentryn31@gmail.com>
2019-08-09 00:28:44 -0500

    Security Update

--- src/index.js
+++ src/index.js
@@ -4,15 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
+import firebaseConfig from "./firebase-config.js";
 
-var config = {
-    apiKey: "AIzaSyAXZzokJ1z36XhIBXUEW_D4Nmy6Ka_gxKE",
-    authDomain: "accuracy-fbla.firebaseapp.com",
-    databaseURL: "https://accuracy-fbla.firebaseio.com",
-    projectId: "accuracy-fbla",
-    storageBucket: "accuracy-fbla.appspot.com",
-    messagingSenderId: "945856739408"
-};
 firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
